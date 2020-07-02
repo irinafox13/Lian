6c42e0bf59702dbc741cf18488a3b958e5bdb9e7
irinafox13 <irinafox93@gmail.com>
2020-07-02 13:56:59 +0300

    data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-	
-
-   
+$(function(){
+	$('.load-more').on('click', function(){
+      const btn = $(this);
+      const loader = btn.find('span');
+      $.ajax({
+         url: '/data.html',
+         type: 'GET',
+         beforeSend: function(){
+            btn.attr('disabled', true);
+            loader.addClass('d-inline-block');
+         },
+         success: function(response){
+            setTimeout(function(){
+               loader.removeClass('d-inline-block');
+               btn.attr('disabled', false);
+               console.log(responce);
+            }, 1000);
+         },
+         error: function(){
+            alert('Error!');
+            loader.removeClass('d-inline-block');
+            btn.attr('disabled', false);
+         }
+      });
+   });   
 });   
